bf3cb8088fc1ffb1be0e432a095f05ae8d0dfeec
Mary Lang <mcl86@cornell.edu>
2020-02-15 18:11:10 -0700

    Add getpip file

--- ican2/settings.py
+++ ican2/settings.py
@@ -124,7 +124,7 @@ client_encoding: 'UTF8'
 
 default_transaction_isolation: 'read committed'
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=600,ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 # Static files (CSS, JavaScript, Images)
